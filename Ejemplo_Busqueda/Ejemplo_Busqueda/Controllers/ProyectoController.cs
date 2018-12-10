05f60c44821e7c410ce02354985e359ea05607f9
Alejandra Beatriz Batres Chicas <alejandra.batres@sv.cds>
2018-12-10 11:15:34 -0600

    ejemplo

---
+++ Ejemplo_Busqueda/Ejemplo_Busqueda/Controllers/ProyectoController.cs
@@ -0,0 +1,76 @@
+﻿using Ejemplo_Busqueda.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Ejemplo_Busqueda.Controllers
+{
+    public class ProyectoController : Controller
+    {
+        Estudiantes Db = new Estudiantes();
+        // GET: Proyecto
+
+        public ActionResult Index()
+        {
+            var listado = Db.lista();
+            return View(listado.ToList());
+        }
+
+        [HttpPost]
+        public ActionResult Index(string txtbuscar, string busc, string rd)
+        {
+            var listado = Db.lista();
+            string formato = txtbuscar.Trim();
+            var query = from a in Db.lista() select a;
+            #region usandoSelec
+            if (formato !="")
+            {
+                if (busc =="0")
+                {
+                    if (busc == "1")
+                    {
+                        query = from n in Db.lista() where n.Nombre.Contains(formato) select n; 
+                    }
+                    if (busc == "2")
+                    {
+                        query = from n in Db.lista() where n.Materia.Contains(formato) select n;
+                    }
+
+                    if (busc == "3")
+                    {
+                        double prom = Convert.ToDouble(formato);
+                        query = from n in Db.lista() where n.Promedio == prom select n;
+
+                    }
+                }
+            }
+
+            #endregion
+            #region usandoradio
+            if (rd =="1")
+            {
+                query = from n in Db.lista() where n.Nombre.Contains(formato) select n;
+            }
+
+            if (rd == "2")
+            {
+                query = from n in Db.lista() where n.Materia.Contains(formato) select n;
+
+            }
+            if (rd == "3")
+            {
+                double prom = Convert.ToDouble(formato);
+                query = from n in Db.lista() where n.Promedio == prom select n;
+            }
+            #endregion
+
+           
+            return View(query.ToList());
+
+        }
+            
+          
+    }
+}
---
+++ Ejemplo_Busqueda/Ejemplo_Busqueda/Models/Estudiantes.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Ejemplo_Busqueda.Models
+{
+    public class Estudiantes
+    {
+        public int Carnet { get; set; }
+        public string Nombre { get; set; }
+        public string Materia { get; set; }
+        public double Promedio { get; set; }
+        public DateTime FechaRegistro { get; set; }
+
+        public List<Estudiantes> lista()
+        {
+            var Estudiante1 = new Estudiantes()
+            {
+                Carnet = 001,
+                Nombre = "Marcos",
+                Materia ="Matematica",
+                Promedio = 8.8,
+                FechaRegistro = DateTime.Now
+            };
+            var Estudiante2 = new Estudiantes()
+            {
+                Carnet = 002,
+                Nombre = "Tomas",
+                Materia = "Informatica",
+                Promedio = 9.5,
+                FechaRegistro = DateTime.Now
+
+            };
+
+            var Estudiante3 = new Estudiantes()
+            {
+                Carnet = 003,
+                Nombre = "Antinio",
+                Materia = "Programacion",
+                Promedio = 10,
+                FechaRegistro = DateTime.Now
+
+            };
+
+            var Estudiante4 = new Estudiantes()
+            {
+                Carnet = 004,
+                Nombre = "Santiago",
+                Materia = "EStadistica",
+                Promedio = 7.5,
+                FechaRegistro = DateTime.Now
+
+            };
+
+
+            var Estudiante5 = new Estudiantes()
+            {
+                Carnet = 005,
+                Nombre = "Francisco",
+                Materia = "Fundamento de Programacon",
+                Promedio = 6,
+                FechaRegistro = DateTime.Now
+
+            };
+            return new List<Estudiantes>() { Estudiante1, Estudiante2, Estudiante3, Estudiante4, Estudiante5 };
+
+
+        }
+    }
+}
